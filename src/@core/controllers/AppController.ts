16b0d70481afe8094bb8a7cf58287c77f1271314
lopesdea91 <lopesdea91@gmail.com>
2023-07-25 16:58:10 -0300

    Revert "add json-server"
    This reverts commit 2a416ea0af03979a5e95d582de7d278898533eac.

--- src/@core/controllers/AppController.ts
+++
@@ -1,3 +0,0 @@
-class AppController { }
-
-export const appController = new AppController()
--- src/@core/infra/Http/HttpClient.ts
+++
@@ -1,6 +0,0 @@
-export default interface HttpClient {
-  get: <R>(url: string) => Promise<{ data: R; status: number }>
-  post: <R>(url: string, input: any) => Promise<{ data: R; status: number }>
-  put: <R>(url: string, input: any) => Promise<{ data: R; status: number }>
-  delete: (url: string) => Promise<{ status: number }>
-}
