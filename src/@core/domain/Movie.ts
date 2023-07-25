2a416ea0af03979a5e95d582de7d278898533eac
lopesdea91 <lopesdea91@gmail.com>
2023-07-25 16:57:24 -0300

    add json-server

---
+++ src/@core/domain/Movie.ts
@@ -0,0 +1,12 @@
+export default interface IMovie {
+  id: number
+  backdropPath: string
+  originalLanguage: string
+  originalTitle: string
+  overview: string
+  posterPath: string
+  releaseDate: string
+  title: string
+  voteAverage: number
+  voteCount: number
+}
